8b893c68746fd4ce7adb1d6f7f5a235599d11e57
Maxi Pelizzoni <maxi96p@gmail.com>
2025-10-21 18:50:29 -0300

    Updated navigation graph.

--- app/src/main/java/com/maxidev/wallsplash/feature/navigation/NavigationGraph.kt
+++ app/src/main/java/com/maxidev/wallsplash/feature/navigation/NavigationGraph.kt
@@ -8,9 +8,9 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.maxidev.wallsplash.feature.collections.presentation.CollectionScreen
 import com.maxidev.wallsplash.feature.detail.presentation.PhotoDetailScreen
-import com.maxidev.wallsplash.feature.favorite.presentation.FavoriteScreen
-import com.maxidev.wallsplash.feature.photos.presentation.PhotosScreen
-import com.maxidev.wallsplash.feature.search.presentation.SearchScreen
+import com.maxidev.wallsplash.feature.favorite.presentation.favoriteDestination
+import com.maxidev.wallsplash.feature.photos.presentation.photosDestination
+import com.maxidev.wallsplash.feature.search.presentation.searchDestination
 import com.maxidev.wallsplash.feature.settings.presentation.SettingsScreen
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -19,23 +19,23 @@ fun NavigationGraph(
     navController: NavHostController = rememberNavController(),
     starDestinations: Destinations = Destinations.PhotosView
 ) {
+    // TODO: Transitions screen
+
     NavHost(
         navController = navController,
         startDestination = starDestinations
     ) {
-        composable<Destinations.PhotosView> {
-            PhotosScreen(
-                navigateToPhotoDetail = { id ->
-                    navController.navigate(Destinations.PhotoDetailsView(id))
-                },
-                navigateToCollections = { id ->
-                    navController.navigate(Destinations.CollectionsView(id))
-                },
-                navigateToSearch = { navController.navigate(Destinations.SearchView) },
-                navigateToFavorites = { navController.navigate(Destinations.FavouritesView) },
-                navigateToSettings = { navController.navigate(Destinations.PreferencesView) }
-            )
-        }
+        photosDestination(
+            navigateToPhotoDetail = { id ->
+                navController.navigate(Destinations.PhotoDetailsView(id))
+            },
+            navigateToCollections = { id ->
+                navController.navigate(Destinations.CollectionsView(id))
+            },
+            navigateToSearch = { navController.navigate(Destinations.SearchView) },
+            navigateToFavorites = { navController.navigate(Destinations.FavouritesView) },
+            navigateToSettings = { navController.navigate(Destinations.PreferencesView) }
+        )
         composable<Destinations.CollectionsView> {
             CollectionScreen(
                 popBack = { navController.popBackStack() },
@@ -44,16 +44,17 @@ fun NavigationGraph(
                 }
             )
         }
-        composable<Destinations.SearchView> {
-            SearchScreen(
-                navigateToDetail = { id ->
-                    navController.navigate(Destinations.PhotoDetailsView(id))
-                }
-            )
-        }
-        composable<Destinations.FavouritesView> {
-            FavoriteScreen()
-        }
+        searchDestination(
+            navigateToDetail = { id ->
+                navController.navigate(Destinations.PhotoDetailsView(id))
+            },
+            navigateToCollections = { id ->
+                navController.navigate(Destinations.CollectionsView(id))
+            }
+        )
+
+        favoriteDestination()
+
         composable<Destinations.PreferencesView> {
             SettingsScreen()
         }
