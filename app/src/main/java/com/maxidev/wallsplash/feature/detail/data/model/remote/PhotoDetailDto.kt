41c93d077c8ca1bdcfa6f526329b0fd83164608d
Maxi Pelizzoni <maxi96p@gmail.com>
2025-10-23 17:30:02 -0300

    Some changes to the data layer on the details screen.

--- app/src/main/java/com/maxidev/wallsplash/feature/detail/data/model/remote/PhotoDetailDto.kt
+++ app/src/main/java/com/maxidev/wallsplash/feature/detail/data/model/remote/PhotoDetailDto.kt
@@ -8,32 +8,26 @@ data class PhotoDetailDto(
     val id: String? = "",
     val width: Int? = 0,
     val height: Int? = 0,
-    val color: String? = "",
     @SerialName("blur_hash")
     val blurHash: String? = "",
     @SerialName("alt_description")
     val altDescription: String? = "",
     val urls: UrlsDto? = UrlsDto(),
     val likes: Int? = 0,
+    val views: Int? = 0,
+    val downloads: Int? = 0,
     val user: UserDto? = UserDto(),
     val exif: ExifDto? = ExifDto(),
-    val location: LocationDto? = LocationDto(),
-    val tags: List<TagDto?>? = listOf(),
-    val views: Int? = 0,
-    val downloads: Int? = 0
+    val tags: List<TagDto?>? = listOf()
 ) {
     @Serializable
     data class UrlsDto(
-        val raw: String? = "",
         val full: String? = "",
         val regular: String? = "",
-        val small: String? = "",
-        val thumb: String? = ""
     )
 
     @Serializable
     data class UserDto(
-        val id: String? = "",
         val username: String? = "",
         val name: String? = "",
         val links: UserLinksDto? = UserLinksDto(),
@@ -49,9 +43,7 @@ data class PhotoDetailDto(
 
     @Serializable
     data class ExifDto(
-        val make: String? = "",
         val model: String? = "",
-        val name: String? = "",
         @SerialName("exposure_time")
         val exposureTime: String? = "",
         val aperture: String? = "",
@@ -59,19 +51,6 @@ data class PhotoDetailDto(
         val focalLength: String? = "",
         val iso: Int? = 0
     )
-
-    @Serializable
-    data class LocationDto(
-        val city: String? = "",
-        val country: String? = "",
-        val position: PositionDto? = PositionDto()
-    ) {
-        @Serializable
-        data class PositionDto(
-            val latitude: Double? = 0.0,
-            val longitude: Double? = 0.0
-        )
-    }
 
     @Serializable
     data class TagDto(val title: String? = "")
